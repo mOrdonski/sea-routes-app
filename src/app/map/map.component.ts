ab21e436ac35b1821572342bf1ad5b7355e9b91e
Maciej <maciejordonski@gmail.com>
2023-12-14 20:21:45 +0100

    feat(map): mam init

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent implements AfterViewInit {
+  private map!: L.Map;
+  private centroid: L.LatLngExpression = [0, 0];
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap = (): void => {
+    this.map = L.map('map', {
+      center: this.centroid,
+      zoom: 1,
+    });
+
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 16,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.map);
+  };
+}
